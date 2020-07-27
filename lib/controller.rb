b6fed20ca0bfc4a91f2a3bb0600701c172c4ae49
oumasma <oumasma93@hotmail.fr>
2020-07-28 03:17:24 +0400

    fix_bug

--- lib/controller.rb
+++ lib/controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < Sinatra::Base    # hérite de toutes les fonctionn
   end
 
   post '/gossips/new/' do  
-  	Gossip.new(params["gossips_author"], params["gossips_content"]).save #on crée un gossip et on l'enregistre
+  	Gossip.new(params["gossip_author"], params["gossip_content"]).save #on crée un gossip et on l'enregistre
   	redirect '/' #on redirige après quoi vers la page d'accueil
 	end
 
